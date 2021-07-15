23a1406cd9fc66fc7ea038c6c65615274846d1eb
abdullah-almasry <66563756+abdullah-almasry@users.noreply.github.com>
2021-07-15 11:50:49 +0200

    Update push_notifications.dart

--- lib/src/utils/push_notifications.dart
+++ lib/src/utils/push_notifications.dart
@@ -1,19 +1,19 @@
 import 'dart:developer';
-import 'package:firebase_messaging/firebase_messaging.dart';
+//import 'package:firebase_messaging/firebase_messaging.dart';
 
 class PushNotificationService {
-  final FirebaseMessaging _fcm = FirebaseMessaging.instance;
+ // final FirebaseMessaging _fcm = FirebaseMessaging.instance;
 
-  PushNotificationService._();
-  static final PushNotificationService _instance = PushNotificationService._();
-  static PushNotificationService get instance => _instance;
+//  PushNotificationService._();
+//  static final PushNotificationService _instance = PushNotificationService._();
+//  static PushNotificationService get instance => _instance;
 
-  Future<String?> getToken() => _fcm.getToken();
+//  Future<String?> getToken() => _fcm.getToken();
 
   Future<void> initialise() async {
     try {
-      final settings = await _fcm.requestPermission();
-      log('User granted permission: ${settings.authorizationStatus}');
+    //  final settings = await _fcm.requestPermission();
+    //  log('User granted permission: ${settings.authorizationStatus}');
     } catch (e) {
       log(e.toString());
     }
